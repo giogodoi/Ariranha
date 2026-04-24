0d12f4520c03984a958ee155e295be07c05fa086
Giovane Felipe Godoi Oliveira <giovane.oliveira@estudante.ufla.br>
2026-04-24 17:41:43 -0300

    Fix: PostMapping /cadastrar recap

--- ariranha/src/main/java/br/com/zetta/ariranha/controller/UsuarioController.java
+++ ariranha/src/main/java/br/com/zetta/ariranha/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import br.com.zetta.ariranha.model.Usuario;
 import br.com.zetta.ariranha.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import br.com.zetta.ariranha.dto.UsuarioCadastroDTO;
 
 import java.util.List;
 
@@ -14,10 +15,17 @@ public class UsuarioController {
     @Autowired
     private UsuarioService service;
 
-    @PostMapping
-    public Usuario criar(@RequestBody Usuario usuario) {
-        return service.salvar(usuario);
-    }
+    @PostMapping("/cadastrar")
+    public Usuario criar(@RequestBody UsuarioCadastroDTO dto) {
+    Usuario usuario = new Usuario();
+    usuario.setNomeCompleto(dto.getNomeCompleto());
+    usuario.setEmail(dto.getEmail());
+    usuario.setCpf(dto.getCpf());
+    usuario.setTelefone(dto.getTelefone());
+    usuario.setSenha(dto.getSenha());
+    
+    return service.salvar(usuario);
+}
 
     @PostMapping("/login")
     public String login(@RequestBody Usuario loginData) {
