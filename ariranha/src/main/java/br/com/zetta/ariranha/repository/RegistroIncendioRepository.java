f648fe9e5498a2cead746240455dc1ddbc48db85
Giovane Godoi <giovane.oliveira@estudante.ufla.br>
2026-04-25 16:17:08 -0300

    Delete ariranha directory

--- ariranha/src/main/java/br/com/zetta/ariranha/repository/RegistroIncendioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.zetta.ariranha.repository;
-import br.com.zetta.ariranha.model.RegistroIncendio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegistroIncendioRepository extends JpaRepository<RegistroIncendio, Long> {
-}
